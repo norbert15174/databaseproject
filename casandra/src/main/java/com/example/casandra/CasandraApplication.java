0ce6e6392a4c29631a080c3107e0908b9f3d04c7
Faron Norbert <faronnorbertkrk@gmail.com>
2022-11-21 20:19:45 +0100

    init casandra

---
+++ casandra/src/main/java/com/example/casandra/CasandraApplication.java
@@ -0,0 +1,13 @@
+package com.example.casandra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CasandraApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CasandraApplication.class , args);
+    }
+
+}
